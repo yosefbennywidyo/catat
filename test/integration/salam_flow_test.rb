2b341eaa0e5690139e441d1e0bc73969112d402c
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-11 05:19:16 +0000

    Added test and switched database encoding * test: Dapat melihat halaman Salam * switch unicode to SQL_ASCI

--- test/integration/salam_flow_test.rb
+++ test/integration/salam_flow_test.rb
@@ -4,4 +4,8 @@ class SalamFlowTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "Dapat melihat halaman Salam" do
+    get "/"
+    assert_select "h1", "Salam, Anda berada di Catel"
+  end
 end
