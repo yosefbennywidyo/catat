f0c14bb03e7a2cad98d31b2e8fa87983eafa0883
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-23 09:04:50 +0000

    Migrated to remove pengaman coloumn from SuratMasuk

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023090053) do
+ActiveRecord::Schema.define(version: 20171023090300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20171023090053) do
     t.string "no_agenda"
     t.string "status"
     t.string "sifat"
-    t.string "pengaman"
     t.string "dari"
     t.string "perihal"
     t.datetime "created_at", null: false
