93d7316adfd6b0265ca1728d1f9b99677225f7a0
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-24 07:07:17 +0000

    Updated error messages * Add validation in surat_masuk.rb * Change error message in _form.html.haml * Change default blank error in en.yml

--- app/models/surat_masuk.rb
+++ app/models/surat_masuk.rb
@@ -1,7 +1,9 @@
 class SuratMasuk < ApplicationRecord
-  validates :status, presence: true, length: { minimum: 4 }
-  validates :sifat, presence: true, length: { minimum: 5 }
-  validates :derajat_pengaman, presence: true, length: { minimum: 5 }
-  validates :dari, presence: true, length: { minimum: 5 }
-  validates :perihal, presence: true, length: { minimum: 5 }
+  validates :nomor_surat, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+    message: "hanya diperkenankan terdiri dari rangkaian huruf dan angka serta karakter / (garis miring) dan . (titik)" }
+  validates :status, presence: true, length: { minimum: 4, message: "harus terdiri dari minimal 4 karakter" }
+  validates :sifat, presence: true, length: { minimum: 5, message: "harus terdiri dari minimal 5 karakter" }
+  validates :derajat_pengaman, presence: true, length: { minimum: 5, message: "harus terdiri dari minimal 5 karakter"  }
+  validates :dari, presence: true, length: { minimum: 5, message: "harus terdiri dari minimal 5 karakter"  }
+  validates :perihal, presence: true, length: { minimum: 5, message: "harus terdiri dari minimal 5 karakter"  }
 end
