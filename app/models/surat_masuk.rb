ab4073b9de28a918db981228a11986819dfbdab7
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-24 06:27:28 +0000

    Added SuratMasuk validation

--- app/models/surat_masuk.rb
+++ app/models/surat_masuk.rb
@@ -1,2 +1,7 @@
 class SuratMasuk < ApplicationRecord
+  validates :status, presence: true, length: { minimum: 4 }
+  validates :sifat, presence: true, length: { minimum: 5 }
+  validates :derajat_pengaman, presence: true, length: { minimum: 5 }
+  validates :dari, presence: true, length: { minimum: 5 }
+  validates :perihal, presence: true, length: { minimum: 5 }
 end
