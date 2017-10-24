0d13dad0506a27e900114a46ffc1828f3b576266
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-24 07:21:36 +0000

    Updated regex code

--- app/models/surat_masuk.rb
+++ app/models/surat_masuk.rb
@@ -1,5 +1,5 @@
 class SuratMasuk < ApplicationRecord
-  validates :nomor_surat, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+  validates :nomor_surat, presence: true, format: { with: /([\/\.A-Za-z0-9])\w+/,
     message: "hanya diperkenankan terdiri dari rangkaian huruf dan angka serta karakter / (garis miring) dan . (titik)" }
   validates :status, presence: true, length: { minimum: 4, message: "harus terdiri dari minimal 4 karakter" }
   validates :sifat, presence: true, length: { minimum: 5, message: "harus terdiri dari minimal 5 karakter" }
