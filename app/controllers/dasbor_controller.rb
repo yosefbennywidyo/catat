d0966dd2c3befea4524ec71ac7b3a04c710e54d1
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-25 02:30:04 +0000

    Updated Dashboard index * Added Pustaka at view and controller

--- app/controllers/dasbor_controller.rb
+++ app/controllers/dasbor_controller.rb
@@ -2,5 +2,6 @@ class DasborController < ApplicationController
   def index
     @jumlah_surat_keluar = SuratKeluar.count
     @jumlah_surat_masuk = SuratMasuk.count
+    @jumlah_pustaka = DaftarPustaka.count
   end
 end
