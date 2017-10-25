ac2f724a42826224ee4e7d0067a29bfc78b2148e
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-25 03:07:03 +0000

    Added DaftarPustaka validations

--- app/models/daftar_pustaka.rb
+++ app/models/daftar_pustaka.rb
@@ -1,2 +1,4 @@
 class DaftarPustaka < ApplicationRecord
+  validates :judul, presence: true, length: { minimum: 10, message: "harus terdiri dari minimal 10 karakter - contoh: Surat Ijin" }
+  validates :keterangan, presence: true, length: { minimum: 10, message: "harus terdiri dari minimal 10 karakter - contoh: Untuk pengajuan ijin, maksimal satu hari setelah tidak masuk" }
 end
