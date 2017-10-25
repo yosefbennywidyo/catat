8b5ea713e1d29af112dd7ee36b529c31f8f35bd0
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-25 01:53:20 +0000

    Added validation for SuratKeluar

--- app/models/surat_keluar.rb
+++ app/models/surat_keluar.rb
@@ -1,2 +1,4 @@
 class SuratKeluar < ApplicationRecord
+  validates :judul, presence: true, length: { minimum: 8, message: "harus terdiri dari minimal 8 karakter - contoh: Undangan" }
+  validates :keterangan, presence: true, length: { minimum: 8, message: "harus terdiri dari minimal 8 karakter - contoh: Untuk kegiatan HAB pada tanggal 3 Januari berlokasi di Aula Kantor Kementerian Agama Sumba Tengah" }
 end
