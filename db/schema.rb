38ef9f0b3e458380bcdf88c3f880c9752f96e533
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-25 02:06:27 +0000

    Migrated DaftarPustaka * rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024081205) do
+ActiveRecord::Schema.define(version: 20171025020426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "daftar_pustaka", force: :cascade do |t|
+    t.string "judul"
+    t.text "keterangan"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "surat_keluar", force: :cascade do |t|
     t.string "judul"
