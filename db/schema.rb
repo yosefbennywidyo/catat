d68a9adfe3daafc54fb3a9325db0e2dcd39d9276
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-25 06:06:47 +0000

    Migrated StatusKepegawaian * rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025055135) do
+ActiveRecord::Schema.define(version: 20171025060428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20171025055135) do
 
   create_table "pengguna", force: :cascade do |t|
     t.string "nama"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "status_kepegawaian", force: :cascade do |t|
+    t.string "status_kepegawaian"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
