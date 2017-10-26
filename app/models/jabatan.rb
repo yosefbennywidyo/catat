4641cb740f3e47be80a262cd24509128faeb9591
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-26 05:38:35 +0000

    Added Jabatan and Pengguna associations

--- app/models/jabatan.rb
+++ app/models/jabatan.rb
@@ -1,3 +1,4 @@
 class Jabatan < ApplicationRecord
   validates :nama, presence: true, length: { minimum: 7, message: "harus terdiri dari minimal 7 karakter - contoh: Penyuluh" }
+  has_and_belongs_to_many :pengguna
 end
--- app/models/pengguna.rb
+++ app/models/pengguna.rb
@@ -1,4 +1,5 @@
 class Pengguna < ApplicationRecord
   validates :nama, presence: true, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter - contoh: Rifaid" }
   has_and_belongs_to_many :status_kepegawaian
+  has_and_belongs_to_many :jabatan
 end
