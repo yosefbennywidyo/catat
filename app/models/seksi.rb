9b2c3b4db23c8adf3883fb85a077a16e0b0d70af
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-27 03:30:20 +0000

    Added Seksi validation

--- app/models/seksi.rb
+++ app/models/seksi.rb
@@ -1,3 +1,4 @@
 class Seksi < ApplicationRecord
+  validates :nama, presence: true, length: { minimum: 19, message: "harus terdiri dari minimal 19 karakter - contoh: Sekretariat Jendral" }
   has_and_belongs_to_many :pengguna
 end
