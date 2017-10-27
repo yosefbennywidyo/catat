dc5c26edb137671eb337eaf31b56177363b36149
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-27 04:50:33 +0000

    Updated jabatan validation

--- app/models/jabatan.rb
+++ app/models/jabatan.rb
@@ -1,4 +1,4 @@
 class Jabatan < ApplicationRecord
-  validates :nama, presence: true, length: { minimum: 7, message: "harus terdiri dari minimal 7 karakter - contoh: Penyuluh" }
+  validates :nama, presence: true, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter - contoh: Kepala" }
   has_and_belongs_to_many :pengguna
 end
