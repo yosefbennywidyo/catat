befd14318b0208c999dee239d7266ed65c0e5d0f
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-27 03:20:44 +0000

    Migrated * Seksi * Seksi and Pengguna join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026053633) do
+ActiveRecord::Schema.define(version: 20171027031912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,9 +39,20 @@ ActiveRecord::Schema.define(version: 20171026053633) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "pengguna_seksi", id: false, force: :cascade do |t|
+    t.bigint "seksi_id", null: false
+    t.bigint "pengguna_id", null: false
+  end
+
   create_table "pengguna_status_kepegawaian", id: false, force: :cascade do |t|
     t.bigint "status_kepegawaian_id", null: false
     t.bigint "pengguna_id", null: false
+  end
+
+  create_table "seksi", force: :cascade do |t|
+    t.string "nama"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "status_kepegawaian", force: :cascade do |t|
