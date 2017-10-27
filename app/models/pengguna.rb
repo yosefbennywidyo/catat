3bf32bef2dcce010083b1bcf192fbf6f0104a0bb
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-27 03:24:47 +0000

    Updated StatusKepegawaian, Jabatan, and Seksi with Pengguna association

--- app/models/pengguna.rb
+++ app/models/pengguna.rb
@@ -1,4 +1,6 @@
 class Pengguna < ApplicationRecord
   validates :nama, presence: true, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter - contoh: Rifaid" }
-  has_and_belongs_to_many :status_kepegawaian, :jabatan, :seksi
+  has_and_belongs_to_many :status_kepegawaian
+  has_and_belongs_to_many :jabatan
+  has_and_belongs_to_many :seksi
 end
