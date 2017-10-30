09fff29566e71dc443c494bb9b36af4c6c909d23
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-30 04:19:35 +0000

    Created Komentar controller and view

---
+++ app/controllers/komentar_controller.rb
@@ -0,0 +1,28 @@
+class KomentarController < ApplicationController
+before_action :find_commentable
+
+    def new
+      @komentar = Komentar.new
+    end
+
+    def create
+      @komentar = @commentable.komentar.new komentar_params
+
+      if @komentar.save
+        redirect_to :back, notice: 'Komentar Anda berhasil dibuat!'
+      else
+        redirect_to :back, notice: "Komentar Anda tidak berhasil dibuat!"
+      end
+    end
+
+    private
+
+    def komentar_params
+      params.require(:komentar).permit(:isi)
+    end
+
+    def find_commentable
+      @commentable = Komentar.find_by_id(params[:komentar_id]) if params[:komentar_id]
+      @commentable = SuratKeluar.find_by_id(params[:surat_keluar_id]) if params[:surat_keluar_id]
+    end
+end
