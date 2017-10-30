61c45dae474d7d2b636d058b3d094aacda58be06
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-10-30 05:11:27 +0000

    Migrated Komentar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027031912) do
+ActiveRecord::Schema.define(version: 20171030034639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20171027031912) do
   create_table "jabatan_pengguna", id: false, force: :cascade do |t|
     t.bigint "jabatan_id", null: false
     t.bigint "pengguna_id", null: false
+  end
+
+  create_table "komentar", force: :cascade do |t|
+    t.text "isi"
+    t.integer "commentable_id"
+    t.string "commentable_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pengguna", force: :cascade do |t|
