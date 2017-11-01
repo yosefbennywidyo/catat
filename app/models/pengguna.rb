f93ad24556a43b9507cdbaf4b53e5e31bffeab2e
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-01 07:11:13 +0000

    Updated Pengguna Model * Adds methods to set and authenticate against a BCrypt password

--- app/models/pengguna.rb
+++ app/models/pengguna.rb
@@ -1,4 +1,6 @@
 class Pengguna < ApplicationRecord
+  has_secure_password
+  
   validates :nama, presence: true, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter - contoh: Rifaid" }
   has_and_belongs_to_many :status_kepegawaian
   has_and_belongs_to_many :jabatan
