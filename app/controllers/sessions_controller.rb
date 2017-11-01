1dc031bc88e5c91d374c2554ab61ef12ef7a6e55
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-01 07:49:45 +0000

    Updated Sessions controller * Defined create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    pengguna = Pengguna.find_by_nama(params[:nama])
+    if pengguna && pengguna.authenticate(params[:password])
+      session[:pengguna_id] = pengguna.id
+      redirect_to dasbor_index_path, notice: "Anda telah berhasil masuk Catel!"
+    else
+      flash.now.alert = "Nama atau password tidak valid"
+      render "new"
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Anda berhasil keluar, terima kasih telah mengggunakan Catel!"
+  end
 end
