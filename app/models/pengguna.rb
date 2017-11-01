bce9beac5c300a0762319dc2d40ff4cf2ca0c726
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-01 07:17:45 +0000

    Added Password validation * Minimal 6 character

--- app/models/pengguna.rb
+++ app/models/pengguna.rb
@@ -2,6 +2,9 @@ class Pengguna < ApplicationRecord
   has_secure_password
   
   validates :nama, presence: true, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter - contoh: Rifaid" }
+  
+  validates :password, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter" }
+  
   has_and_belongs_to_many :status_kepegawaian
   has_and_belongs_to_many :jabatan
   has_and_belongs_to_many :seksi
