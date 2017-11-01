d1ae0ea328892a2e9035ab966a737f66268bcc7f
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-01 07:15:05 +0000

    Migrated password_digest to Pengguna * rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031084400) do
+ActiveRecord::Schema.define(version: 20171101071247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171031084400) do
     t.string "nama"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "pengguna_seksi", id: false, force: :cascade do |t|
