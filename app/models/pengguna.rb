e02059533d7f0e4e706ee8d740d3c48796efb2a9
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-01 07:26:53 +0000

    Updated Pengguna model * Added attr_accessible and validates_uniqueness_of

--- app/models/pengguna.rb
+++ app/models/pengguna.rb
@@ -1,6 +1,4 @@
 class Pengguna < ApplicationRecord
-  has_secure_password
-  
   validates :nama, presence: true, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter - contoh: Rifaid" }
   
   validates :password, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter" }
@@ -8,4 +6,10 @@ class Pengguna < ApplicationRecord
   has_and_belongs_to_many :status_kepegawaian
   has_and_belongs_to_many :jabatan
   has_and_belongs_to_many :seksi
+  
+  has_secure_password
+  
+  attr_accessible :nama, :password, :password_confirmation
+  
+  validates_uniqueness_of :nama
 end
