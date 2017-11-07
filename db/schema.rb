05ee8d573b8172259bd31343e997fe0524ed6501
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-07 03:33:21 +0000

    Migrated add Status to Pengguna

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107005226) do
+ActiveRecord::Schema.define(version: 20171107033217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20171107005226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "status"
   end
 
   create_table "pengguna_seksi", id: false, force: :cascade do |t|
