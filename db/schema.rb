d2e099e7ec9cd17dee683d2b3635ac4ea775beb7
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-08 08:43:08 +0000

    Migrated StatusAkun

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108082830) do
+ActiveRecord::Schema.define(version: 20171108083239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,12 @@ ActiveRecord::Schema.define(version: 20171108082830) do
 
   create_table "seksi", force: :cascade do |t|
     t.string "nama"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "status_akun", force: :cascade do |t|
+    t.string "keterangan"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
