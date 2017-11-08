20f383330b2b48d574990026d1572f44b6ef1be0
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-08 09:55:55 +0000

    Removed default value from StatusAkun

--- app/models/status_akun.rb
+++ app/models/status_akun.rb
@@ -1,5 +1,4 @@
 class StatusAkun < ApplicationRecord
-  attribute :keterangan, :string, default: "Tidak aktif"
   validates :keterangan, presence: true, length: { minimum: 5, message: "harus terdiri dari minimal 5 karakter - contoh: Aktif" }
   has_and_belongs_to_many :pengguna
 end
