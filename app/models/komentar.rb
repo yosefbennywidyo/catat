027e2fad4ad050e84b4a5c58e8796bd6c6916cad
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-10 05:41:55 +0000

    Added komentar validations

--- app/models/komentar.rb
+++ app/models/komentar.rb
@@ -1,4 +1,6 @@
 class Komentar < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   has_many :komentar, as: :commentable
+  validates :isi, presence: true, length: { minimum: 2, message: "harus terdiri dari minimal 2 karakter - contoh: Ok" }
+
 end
