10aeaecec019a9cd3deff21c88b42d4f962dba22
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-11 01:05:24 +0000

    Added LampiranDokumen to SuratKeluar

--- app/models/surat_keluar.rb
+++ app/models/surat_keluar.rb
@@ -1,4 +1,5 @@
 class SuratKeluar < ApplicationRecord
+  has_attached_file :lampiran_dokumen
   has_many :komentar, as: :commentable
   validates :judul, presence: true, length: { minimum: 8, message: "harus terdiri dari minimal 8 karakter - contoh: Undangan" }
   validates :keterangan, presence: true, length: { minimum: 8, message: "harus terdiri dari minimal 8 karakter - contoh: Untuk kegiatan HAB pada tanggal 3 Januari berlokasi di Aula Kantor Kementerian Agama Sumba Tengah" }
