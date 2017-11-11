dc1d7e6114880d285b2989982bce7ffb40214c53
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-11 01:09:50 +0000

    Added LampiranDokumen to SuratKeluar params

--- app/controllers/surat_keluar_controller.rb
+++ app/controllers/surat_keluar_controller.rb
@@ -71,6 +71,6 @@ class SuratKeluarController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def surat_keluar_params
-      params.require(:surat_keluar).permit(:judul, :keterangan, :status_surat)
+      params.require(:surat_keluar).permit(:judul, :keterangan, :status_surat, :lampiran_dokumen)
     end
 end
