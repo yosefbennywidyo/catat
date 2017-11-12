189341a3e026a04a4ccf633cf49bc0c0bace4b7b
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-12 18:10:07 +0000

    Remove AddLampiranToKomentar migration

--- db/migrate/20171112165247_add_lampiran_to_komentar.rb
+++
@@ -1,9 +0,0 @@
-class AddLampiranToKomentar < ActiveRecord::Migration[5.1]
-  def up
-    add_attachment :komentar, :lampiran
-  end
-
-  def down
-    remove_attachment :komentar, :lampiran
-  end
-end
