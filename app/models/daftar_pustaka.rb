5bbe0d66514a712e483f75d2fba47f82bd98a925
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-13 07:52:03 +0000

    Added Lampiran and Komentar

--- app/models/daftar_pustaka.rb
+++ app/models/daftar_pustaka.rb
@@ -1,4 +1,8 @@
 class DaftarPustaka < ApplicationRecord
+  has_attached_file :lampiran
+  has_many :komentar, as: :commentable
   validates :judul, presence: true, length: { minimum: 10, message: "harus terdiri dari minimal 10 karakter - contoh: Surat Ijin" }
   validates :keterangan, presence: true, length: { minimum: 10, message: "harus terdiri dari minimal 10 karakter - contoh: Untuk pengajuan ijin, maksimal satu hari setelah tidak masuk" }
+  validates_attachment :lampiran
+  do_not_validate_attachment_file_type :lampiran
 end
