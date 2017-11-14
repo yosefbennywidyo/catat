c11b613adaa82aab0467f240ea5b145a56bc24ef
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-14 03:45:05 +0000

    Migrated DerajatPengaman and SuratMasuk join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114030903) do
+ActiveRecord::Schema.define(version: 20171114034324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 20171114030903) do
     t.string "lampiran_content_type"
     t.integer "lampiran_file_size"
     t.datetime "lampiran_updated_at"
+  end
+
+  create_table "derajat_pengaman", force: :cascade do |t|
+    t.string "nama"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "derajat_pengaman_surat_masuk", id: false, force: :cascade do |t|
+    t.bigint "derajat_pengaman_id", null: false
+    t.bigint "surat_masuk_id", null: false
   end
 
   create_table "jabatan", force: :cascade do |t|
