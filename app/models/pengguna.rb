828403e7e680b418d292c0a2007ed9fc2476d997
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-22 06:04:15 +0000

    Generated Devise views * rails generate devise:views

--- app/models/pengguna.rb
+++ app/models/pengguna.rb
@@ -2,7 +2,7 @@ class Pengguna < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :lockable
   validates :nama, presence: true, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter - contoh: Rifaid" }
   
   validates :password, length: { minimum: 6, message: "harus terdiri dari minimal 6 karakter" }
