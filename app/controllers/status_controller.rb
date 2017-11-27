6d47ddbe0c32a73a01b957740acb120f46dd693b
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-27 04:48:18 +0000

    Added SuratKeluar to Status params

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,4 +1,9 @@
 class StatusController < ApplicationController
   def index
   end
+  
+  private
+  def status_params
+      params.require(:status).permit(:status_type, surat_keluar_ids:[])
+    end
 end
