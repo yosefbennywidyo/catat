a5950cd33576c5d74ec693890c96d02ec5eae5c9
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-28 09:05:20 +0000

    Status channel setting

--- app/channels/status_channel.rb
+++ app/channels/status_channel.rb
@@ -1,12 +1,14 @@
 class StatusChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_from "surat_keluar"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def update
+  def update(data)
+    SuratKeluar.create({ judul: data['judul'], keterangan: data['keterangan'] })
   end
 end
