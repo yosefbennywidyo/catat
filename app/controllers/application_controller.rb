000eb6b3ca76b6c424d76a0239c979903a9c8235
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-11-29 05:50:14 +0000

    Update private for authorize only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
   
-  private
-
   def current_user
     @current_user ||= Pengguna.find(session[:pengguna_id]) if session[:pengguna_id]
   end
   helper_method :current_user
-  
+
+  private
+
   def authorize
     redirect_to login_url, alert: "Anda tidak memiliki hak akses halaman ini!" if current_user.nil?
   end
