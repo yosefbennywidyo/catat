ff25f09a399be1735b4fe720569dbb51473072bb
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-12-05 02:01:17 +0000

    Updated Komentar channel setting * Set subscribed with stream_from "komentar_channel" * Set create(data) with Komentar.create

--- app/channels/komentar_channel.rb
+++ app/channels/komentar_channel.rb
@@ -1,9 +1,15 @@
 class KomentarChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_from "komentar_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+  
+  def create(data)
+    # data is like parameters received from front end
+    Komentar.create({ isi: data['isi'] })
   end
 end
