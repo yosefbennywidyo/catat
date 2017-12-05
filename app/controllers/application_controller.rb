efe69190c00a0f1a288c4cf1a5b4085277bc7b27
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-12-05 06:07:13 +0000

    Removed private and hide_action from Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,11 @@ class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
   protect_from_forgery with: :exception
   
-  private
-
   def current_user
     @current_user ||= Pengguna.find(session[:pengguna_id]) if session[:pengguna_id]
   end
   helper_method :current_user
-  hide_action :current_user
-  
+
   def authorize
     redirect_to login_url, alert: "Anda tidak memiliki hak akses halaman ini!" if current_user.nil?
   end
