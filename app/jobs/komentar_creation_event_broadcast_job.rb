bc91430051a6378f7294c3483b3e142343b610a6
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-12-05 02:09:51 +0000

    Updated Komentar job * Added perform(komentar) * Added render_isi(komentar)

--- app/jobs/komentar_creation_event_broadcast_job.rb
+++ app/jobs/komentar_creation_event_broadcast_job.rb
@@ -1,7 +1,23 @@
 class KomentarCreationEventBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  #def perform(*args)
     # Do something later
+  #end
+  def perform(komentar)
+    # broadcast to komentar_channel with the isi
+    # of the newly created komentar
+    ActionCable.server.broadcast('komentar_channel', {
+      description: render_isi(komentar)
+    })
+  end
+
+  private
+
+  def render_isi(komentar)
+    # In Rails 5, renderer has been made public so that we can use
+    # ApplicationController.renderer to render a partial
+    ApplicationController.renderer.render(
+      partial: 'komentar/komentar', locals: { komentar: komentar })
   end
 end
