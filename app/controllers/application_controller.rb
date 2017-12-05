74dca0da32767d570f4324e851cc94c75480adc4
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-12-05 05:23:38 +0000

    Makes sure that this method is not considered a controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= Pengguna.find(session[:pengguna_id]) if session[:pengguna_id]
   end
   helper_method :current_user
+  hide_action :current_user
   
   def authorize
     redirect_to login_url, alert: "Anda tidak memiliki hak akses halaman ini!" if current_user.nil?
